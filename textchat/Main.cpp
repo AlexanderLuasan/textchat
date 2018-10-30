c7f0ead00ac2d0acded0e15e2fdc94cd46d3a4d6
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2018-10-30 13:26:28 +0800

    hell

--- textchat/Main.cpp
+++ textchat/Main.cpp
@@ -4,14 +4,24 @@
 #include "server.h"
 #include "client.h"
 #include "chatfunc.h"
+#include <thread>
+#include <future>
 
 using namespace std;
+int sendfunc(fd_set sockets);
+int recfunc(fd_set sockets);
 int main() {
 	cout << "Text chat start" << endl;
 	bool chating = true;
 	while (chating) {
+		int choice;
 		cout << "server  or client" << endl;
-		int choice = stoi(getUser());
+		if (getUser() == "s") {
+			choice = 0;
+		}
+		else {
+			choice = 1;
+		}
 
 
 		char host[NI_MAXHOST]; // name
@@ -22,7 +32,9 @@ int main() {
 		sockaddr_in partnerdata;
 		int size = sizeof(partnerdata);
 
-
+		timeval timeout;
+		fd_set sockets;
+		timeout.tv_sec=1;
 		if (choice == 0) {
 			//collect ports n stuff
 			cout << "Enter your own port information." << endl;
@@ -50,55 +62,72 @@ int main() {
 				inet_ntop(AF_INET, &partnerdata.sin_addr, host, NI_MAXHOST);
 				cout << host << " connected on port " << ntohs(partnerdata.sin_port) << endl;
 			}
-			string temp;
-			cout << "<<" << flush;
-			temp = getUser();
-			send(partner, temp.c_str(), temp.length(), 0);
 		}
 
+		sockets.fd_count = 1;
+		sockets.fd_array[0] = partner;
 
 
 		int bitsrecved;
 		char data[4096];
 		string mess;
+
+		future<int> ready = async(&sendfunc, sockets);
+		future<int> ready2 = async(&recfunc, sockets);
+		
 		while (true) {
-			ZeroMemory(data, 4096);
-			bitsrecved = recv(partner, data, 4096, 0);
-			if (bitsrecved == 0) {
-				cout << "quitting" << endl;
-				break;
-			}
-			else if (data[0] == '!'&& data[1] == 'q') {
-				cout << "requesting quit" << endl;
-				char qd[3] = "!q";
-				send(partner, qd, 3, 0);
-				mess = getUser();
-				if (mess.compare("yes") == 0 || mess.compare("y") == 0) {
-					chating = false;
-					break;
-				}
-				else {
-					break;
-				}
+			
+			
+			auto status = ready.wait_for(0ms);
+			
+			if (status==future_status::ready) {
+				ready = async(&sendfunc, sockets);
 			}
 			else {
-				cout << ">>" << data << endl;
+				
 			}
-			cout << "<<" << flush;
-			mess = getUser();
-
-			if (mess.compare("quit") == 0) {
-				char qd[3] = "!q";
-				send(partner, qd, 3, 0);
+			auto status2 = ready2.wait_for(0ms);
+			if (status2 == future_status::ready) {
+				ready2 = async(&recfunc, sockets);
 			}
 			else {
-				send(partner, mess.c_str(), mess.length(), 0);
-			}
 
+			}
 		}
 
 		closesocket(partner);
 	}
 	system("pause");
 	return 0;
+}
+
+int sendfunc(fd_set sockets)
+{
+	string mess;
+	cout << "<<" << flush;
+	mess = getUser();
+
+	if (mess.compare("quit") == 0) {
+		char qd[3] = "!q";
+		send(sockets.fd_array[0], qd, 3, 0);
+	}
+	else {
+		send(sockets.fd_array[0], mess.c_str(), mess.length(), 0);
+	}
+	return 0;
+}
+int recfunc(fd_set sockets) {
+	char data[400];
+	int bitsrecved;
+	
+	ZeroMemory(data, 400);
+	bitsrecved = recv(sockets.fd_array[0], data, 4096, 0);
+	if (bitsrecved == 0) {
+		cout << "quitting" << endl;
+		exit(1);
+	}
+	else {
+		cout << endl << ">>" << data << endl;
+	}
+	return 1;
 }
